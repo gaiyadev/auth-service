0a5ef4cacba9ac87f0458da7feb43e0a32c66dca
Gaiya M. Obed <95748043+gaiyaobed@users.noreply.github.com>
2022-11-12 16:03:02 +0100

    intial commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<any> {
+    try {
+      const user = new User();
+      user.email = createUserDto.email;
+      user.password = createUserDto.password;
+      const saved = await this.userRepository.save(user);
+      return {
+        message: 'added success',
+        data: {
+          id: saved.id,
+          email: saved.email,
+        },
+      };
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
